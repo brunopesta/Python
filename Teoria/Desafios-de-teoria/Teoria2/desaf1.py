709c81871e3ba39376205275ba7d3f990ca5d1a4
Bruno Pestana Carrusca <70908140+pestax@users.noreply.github.com>
2022-03-31 22:04:48 -0300

    a

---
+++ Teoria/Desafios-de-teoria/Teoria2/desaf1.py
@@ -0,0 +1,3 @@
+for i in range(4):
+    cadena = input("Ingresá una palabra")
+    print("TIENE R" if "r" in cadena else "NO TIENE R")
---
+++ Teoria/Desafios-de-teoria/Teoria2/desaf2.py
@@ -0,0 +1,6 @@
+
+cadena = input("Ingrese una palabra ")
+while cadena != "fin":
+    if cadena[0] == cadena[-1]:
+        print(cadena)
+    cadena = input("Ingrese una palabra ")
