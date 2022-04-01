c2dc0facebe1eff03db8ef9e104c80a1d18b9b56
Bruno Pestana Carrusca <70908140+pestax@users.noreply.github.com>
2022-03-31 22:04:21 -0300

    actualizamosss
    aa

---
+++ Practica/Practica1/p1ej1.py
@@ -0,0 +1,17 @@
+import random
+#Variable-
+num_aleatorio = random.randrange(101)
+gane = False
+print("Tenés 5 intentos para adivinar un entre 0 y 100")
+intento = 1
+while (intento <= 5 
+       and not gane):
+    num_Ingresado = int(input('Ingresa tu número: '))
+    if num_Ingresado == num_aleatorio:
+        print(' ganaste y lo hiciste en  {} intentos!!!'.format(intento))
+        gane = True
+    else:
+        print('le pifiaste, Seguí intentando.')
+        intento += 1
+if not gane:
+    print('\n perdiste :(\n El número era: {}'.format(num_aleatorio))
---
+++ Teoria/Desafios-de-teoria/Teoria2/desaf3.py
@@ -0,0 +1,15 @@
+nota = int(input(" Ingresa una nota (-1 para finalizar"))
+while nota != -1:
+    lista_de_notas.append(nota)
+    nota = int(input(" Ingresa una nota (-1 para finalizar"))
+
+cant = len(lista_de_notas)
+suma = 0
+
+for indice in range(cant):
+    suma += lista_de_notas[indice]
+    print(suma)
+if len(lista_de_notas) != 0:
+    print("El promedio es", (suma / len(lista_de_notas)))
+else:
+    print("El promedio es 0")
